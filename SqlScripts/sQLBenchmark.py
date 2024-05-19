497761b310ad45ebcfbab25a586f5112edaa75c6
chamaael@etu.unistra.fr <chamaael@etu.unistra.fr>
2024-05-19 15:08:54 +0200

    SQL Benchmarking

--- SqlScripts/sQLBenchmark.py
+++ SqlScripts/sQLBenchmark.py
@@ -1,65 +1,111 @@
 import time
+import pandas as pd
 import mysql.connector
+import matplotlib.pyplot as plt
 
 # Connexion à MySQL avec le mot de passe
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="my-secret-pw",
-    database="benchmark_db",
-    port=3306
+    password="root",
+    database="elections_benchmark",
+    port=10101
 )
 
-# Créer un curseur pour exécuter les requêtes SQL
-cursor = conn.cursor()
 
-# Benchmark pour les opérations d'ajout
-start_time = time.time()
-for i in range(20000):
-    cursor.execute("""
-    INSERT INTO benchmark_table (uai_siege, crous, uai, personne_referente, fonction, mail, telephone, portable, adresse, lon, lat, com_code, com_nom, uucr_id, uucr_nom, dep_id, dep_nom, aca_id, aca_nom, reg_id, reg_nom, reg_id_old, reg_nom_old, localisation)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """, (
-        "exemple", "exemple", "exemple", "exemple", None, "exemple",
-        "exemple", None, "exemple", 0.0, 0.0,
-        "exemple", "exemple", "exemple", "exemple",
-        "exemple", "exemple", "exemple", "exemple",
-        "exemple", "exemple", "exemple", "exemple",
-        "exemple"
-    ))
+def fileBEnchmark(fileName):
+    # Créer un curseur pour exécuter les requêtes SQL
+    cursor = conn.cursor()
 
-# Valider les changements
-conn.commit()
-end_time = time.time()
-print(f"Temps d'ajout pour 1000 opérations: {end_time - start_time} secondes")
+    # Charger les données du fichier Excel
+    excel_file = fileName
+    df = pd.read_excel(excel_file)
 
-# # Benchmark pour les opérations de mise à jour
-# start_time = time.time()
-# for i in range(20000):
-#     cursor.execute("""
-#     UPDATE benchmark_table SET telephone = %s WHERE uai_siege = %s
-#     """, (
-#         "nouveau_numero", "exemple"
-#     ))
+    # Convertir les types de données à des types Python standard
+    df = df.astype(str)
 
-# Valider les changements
-conn.commit()
-end_time = time.time()
-print(f"Temps de mise à jour pour 1000 opérations: {end_time - start_time} secondes")
+    # Initialiser les listes pour stocker les temps pris pour chaque opération
+    ajout_times = []
+    update_times = []
+    select_times = []
 
-# Benchmark pour les opérations de requête
-start_time = time.time()
-for i in range(20000):
-    cursor.execute("""
-    SELECT * FROM benchmark_table WHERE uai_siege = %s
-    """, (
-        "exemple",
-    ))
-    result = cursor.fetchall()  # Récupérer tous les résultats pour vider le curseur
+    # Benchmark pour les opérations d'ajout
+    start_time = time.time()
+    for i in range(len(df)):
+        row = df.iloc[i]  # Get row data
+        cursor.execute("""
+        INSERT INTO elections (`Code du département`, `Libellé du département`, `Code de la commune`, `Libellé de la commune`, `Etat saisie`, `Inscrits`, `Abstentions`, `% Abs/Ins`, `Votants`, `% Vot/Ins`, `Blancs`, `% Blancs/Ins`, `% Blancs/Vot`, `Nuls`, `% Nuls/Ins`, `% Nuls/Vot`, `Exprimés`, `% Exp/Ins`, `% Exp/Vot`, `N°Panneau`, `Sexe`, `Nom`, `Prénom`, `Voix`, `% Voix/Ins`, `% Voix/Exp`)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            row['Code du département'], row['Libellé du département'], row['Code de la commune'],
+            row['Libellé de la commune'], row['Etat saisie'], row['Inscrits'], row['Abstentions'], row['% Abs/Ins'],
+            row['Votants'], row['% Vot/Ins'], row['Blancs'], row['% Blancs/Ins'], row['% Blancs/Vot'], row['Nuls'],
+            row['% Nuls/Ins'], row['% Nuls/Vot'], row['Exprimés'], row['% Exp/Ins'], row['% Exp/Vot'], row['N°Panneau'],
+            row['Sexe'], row['Nom'], row['Prénom'], row['Voix'], row['% Voix/Ins'], row['% Voix/Exp']
+        ))
 
-end_time = time.time()
-print(f"Temps de requête pour 1000 opérations: {end_time - start_time} secondes")
+    # Valider les changements
+    conn.commit()
+    end_time = time.time()
+    ajout_times.append(end_time - start_time)
 
-# Fermer le curseur et la connexion
-cursor.close()
-conn.close()
+    # Benchmark pour les opérations de mise à jour
+    start_time = time.time()
+    for i in range(len(df)):
+        row = df.iloc[i]  # Get row data
+        cursor.execute("""
+        UPDATE elections SET Voix = %s WHERE `Code du département` = %s
+        """, (
+            row['Voix'], row['Code du département']
+        ))
+
+    # Valider les changements
+    conn.commit()
+    end_time = time.time()
+    update_times.append(end_time - start_time)
+
+    # Benchmark pour les opérations de requête
+    start_time = time.time()
+    for i in range(len(df)):
+        cursor.execute("""
+        SELECT * FROM elections WHERE `Code du département` = %s
+        """, (
+            df.iloc[i]['Code du département'],
+        ))
+        result = cursor.fetchall()  # Récupérer tous les résultats pour vider le curseur
+
+    end_time = time.time()
+    select_times.append(end_time - start_time)
+
+    # Fermer le curseur
+    cursor.close()
+
+    return ajout_times, update_times, select_times
+
+
+if __name__ == '__main__':
+    file_sizes = [1000, 5000, 10000, 20000]
+    ajout_times_list = []
+    update_times_list = []
+    select_times_list = []
+
+    for size in file_sizes:
+        fileName = f'../MOCK_DATA_{size}.xlsx'
+        ajout_times, update_times, select_times = fileBEnchmark(fileName)
+        ajout_times_list.append(sum(ajout_times))
+        update_times_list.append(sum(update_times))
+        select_times_list.append(sum(select_times))
+
+    # Plotting the graph
+    plt.plot(file_sizes, ajout_times_list, label='Ajout')
+    plt.plot(file_sizes, update_times_list, label='Mise à jour')
+    plt.plot(file_sizes, select_times_list, label='Sélection')
+
+    plt.xlabel('Nombre d\'éléments')
+    plt.ylabel('Temps (secondes)')
+    plt.title('Temps pris pour les opérations en fonction du nombre d\'éléments')
+    plt.legend()
+    plt.grid(True)
+        # Enregistrer le graphique
+    plt.savefig('operations_temps.png')
+    plt.show()
